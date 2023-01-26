a683d833396e20befc7dea34e48efdab25548c84
Amjad Afridi <amjadafridi067@gmail.com>
2023-01-27 02:49:13 +0500

    order tailor and nearby tailors added

---
+++ models/customer/orderTailor.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Customer = require("./customer");
+const Tailor = require("../tailor/tailor");
+const TailorService = require("../tailor/tailorService");
+const orderTailorSchema = mongoose.Schema({
+  serviceId: { type: mongoose.Schema.Types.ObjectId, ref: "TailorService" },
+  orderStatus: {
+    type: String,
+  },
+  date: String,
+  customer: { type: mongoose.Schema.Types.ObjectId, ref: "Customer" },
+  pickUpLocation: String,
+  dropUpLocation: String,
+  price: String,
+  measurementType: String,
+  description: String,
+});
+module.exports = mongoose.model("OrderTailor", orderTailorSchema);
